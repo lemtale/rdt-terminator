d8e49b292f57dc465d9e8d3229434b4972f6f1b4
lemtale <lemtale@gmail.com>
2022-08-03 14:05:04 +0100

    Added user inputs

--- rdt-terminator.py
+++ rdt-terminator.py
@@ -1,1 +1,26 @@
-print('hello')
+from getpass import getpass
+import praw
+
+
+def get_reddit_client():
+    client_id = getpass('Client_Id: ')
+    client_secret = getpass('Client_Secret: ')
+    username = input('Username: ')
+    password = getpass('Password: ')
+
+    return praw.Reddit(
+        client_id=client_id,
+        client_secret=client_secret,
+        username=username,
+        password=password,
+        user_agent="RDT Terminator Script"
+    )
+
+def app():
+    reddit = get_reddit_client()
+    for submission in reddit.subreddit("all").hot(limit=25):
+        print(submission.title)
+
+
+if __name__ == '__main__':
+    app()
